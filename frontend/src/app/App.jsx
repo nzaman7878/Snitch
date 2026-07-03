c96f91c43066398119d3d9643a509be9fd40c9e0
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-07-03 14:47:26 +0530

    feat: add react-router for routing and create initial app routes

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
 function App() {
 
   return (
     <>
       <h1>Hello</h1>
+      <h1>Hello world</h1>
+      <RouterProvider router={routes} />
     </>
   )
 }
