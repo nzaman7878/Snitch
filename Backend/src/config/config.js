1b2d743cdb2b471564e18b592693a357559ac239
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-07-06 18:48:06 +0530

    feat: integrate Razorpay for payment processing and add order creation and verification functionality

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -29,6 +29,14 @@ if (!process.env.IMAGEKIT_URL_ENDPOINT) {
     throw new Error("IMAGEKIT_URL_ENDPOINT is not defined in environment variables")
 }
 
+if (!process.env.RAZORPAY_KEY_ID) {
+    throw new Error("RAZORPAY_KEY_ID is not defined in environment variables")
+}
+
+if (!process.env.RAZORPAY_KEY_SECRET) {
+    throw new Error("RAZORPAY_KEY_SECRET is not defined in environment variables")
+}
+
 
 export const config = {
     MONGO_URI: process.env.MONGO_URI,
@@ -38,5 +46,7 @@ export const config = {
     NODE_ENV: process.env.NODE_ENV || "development",
     IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY,
     IMAGEKIT_PUBLIC_KEY: process.env.IMAGEKIT_PUBLIC_KEY,
-    IMAGEKIT_URL_ENDPOINT: process.env.IMAGEKIT_URL_ENDPOINT
+    IMAGEKIT_URL_ENDPOINT: process.env.IMAGEKIT_URL_ENDPOINT,
+        RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET
 }
